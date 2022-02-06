af03211dfc8b2b108d483fbae56fdb01c7c0902e
justinsilvestre <justinsilvestre@gmail.com>
2022-02-06 10:58:18 +0100

    Add comments

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
-import { exportedFunction } from './myModule.ts'
+// make sure this file '.myModule.ts' is not open
+// import { exportedFunction } from './myModule.ts'
 
+// cmd + . should suggest import, but does not.
 exportedFunction()
--- myModule.ts
+++ myModule.ts
@@ -1,3 +1,7 @@
 export function exportedFunction() {
   console.log('hello!')
 }
+
+export function exportedFunction2() {
+  console.log('hello!')
+}
